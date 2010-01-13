cab946a7c770f0a0a1ac21c1c56f8bc11f51b31e
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-01-13 21:30:32 +0100

    + simple model based on dublin core

--- thingsoup/thingsoup/model/__init__.py
+++ thingsoup/thingsoup/model/__init__.py
@@ -1,6 +1,7 @@
 """The application's model objects"""
 import sqlalchemy as sa
 from sqlalchemy import orm
+from uuid import uuid4
 
 from thingsoup.model import meta
 
@@ -34,3 +35,27 @@ def init_model(engine):
 #
 #class Reflected(object):
 #    pass
+
+things_table = sa.Table('things', meta.metadata,
+                sa.Column('dc_identifier', sa.types.String(45), primary_key=True),
+                # uuid.uuid4().urn.__len__() is always 45
+                sa.Column('dc_title', sa.types.Unicode(255), default=''),
+                sa.Column('dc_description', sa.types.Unicode(), default=''),
+                sa.Column('dc_type', sa.types.String())
+                )
+
+
+class Thing(object):
+
+    def __init__(self, title, description='', type='PhysicalObject'):
+        self.dc_identifier = uuid4().urn
+        self.dc_title = title
+        self.dc_description = content
+        self.dc_type = type
+
+    def __unicode__(self):
+        return self.title, self.type
+
+    __str__ = __unicode__
+
+orm.mapper(Thing, things_table)
