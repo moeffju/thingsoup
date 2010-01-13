aa70189edd640ebf7a8bc04fb8f9734a845e1a75
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-01-14 03:57:43 +0800

    + things controller got new dummy actions

--- thingsoup/thingsoup/controllers/things.py
+++ thingsoup/thingsoup/controllers/things.py
@@ -13,4 +13,16 @@ class ThingsController(BaseController):
         # Return a rendered template
         #return render('/things.mako')
         # or, return a response
-        return 'Hello World'
+        return 'Goodbye, cruel world.'
+
+    def create(self):
+        # create a new object
+        return 'dummy: object created'
+
+    def new(self):
+        # show new object form
+        return 'dummy: new object form'
+
+    def show(self):
+        # show an object
+        return 'dummy: details about object'
